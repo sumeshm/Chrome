e9428f81897ab88a5dcedd85e5c2fb70b2e3cca2
Sumesh <smani5@BANL160135668.local>
2018-08-17 15:55:10 +0530

    implemnted add, remove, clear, save and restore of tiles for tab page

--- NewTabTiles/buttonAction.js
+++ NewTabTiles/buttonAction.js
@@ -1,59 +1,111 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 	// populate tiles
-	init();
+	// readInputFile();
+	readFromStorage();
 
-	// tile_buttons
-	var tileButtonList = document.querySelectorAll(".tile_button");
-	for (i = 0; i < tileButtonList.length; i++) {
-		tileButtonList[i].addEventListener('click', tileButtonClick);
+	// add_button and home_button click listener
+	var addButtons = document.querySelectorAll('.add_button');
+	logMessage("init: ", "addButtons.length=" + addButtons.length);
+	for (i = 0; i < addButtons.length; i++) {
+		addButtons[i].addEventListener('click', process_buttonClick);
 	}
 
-	// add_button and home_button
-	document.querySelector('.home_button').addEventListener('click',
-			buttonClick);
-	document.querySelector('.add_button')
-			.addEventListener('click', buttonClick);
-
-	// book-marks list
+	// book-marks list select listener
 	document.getElementById("select_bookmarks").addEventListener('change',
 			process_listChange);
 
+	// book-marks list loaded listener
 	chrome.bookmarks.getTree(process_bookmark);
 });
 
-function init() {
-	var rawFile = new XMLHttpRequest();
-	rawFile.open("GET", 'url.json', true);
-	rawFile.onreadystatechange = function() {
-		if (rawFile.readyState === 4) {
-			var fileContents = rawFile.responseText;
-			var jsonArray = JSON.parse(fileContents);
-			logMessage("Init: ", "jsonArray=" + jsonArray.length);
+// write tiles list (JSON array) to storage
+function writeToStorage(jsonArray) {
+	var TilesInfoKey = 'TilesInfoKey';
+	var jsonArrayStr = JSON.stringify(jsonArray);
+	logMessage("writeToStorage: ", "tileInfoStr=" + jsonArrayStr);
 
-			var tiles_grid = document.getElementById("tiles_grid");
-
-			for (i = 0; i < jsonArray.length; i++) {
-				var tileInfo = jsonArray[i];
-				logMessage("Init: ", "tileInfo.title=" + tileInfo.title);
-				logMessage("Init: ", "tileInfo.url=" + tileInfo.url);
-
-				var button = document.createElement("button");
-				button.className = "tile_button";
-				button.value = tileInfo.url;
-				button.innerText = tileInfo.title;
-				button.addEventListener('click', tileButtonClick);
-
-				tiles_grid.appendChild(button);
-				logMessage("Init: ", "updated HTML");
-			}
-		}
-	}
-
-	rawFile.send();
+	chrome.storage.local.set({
+		TilesInfoKey : jsonArrayStr
+	}, function() {
+		logMessage("writeToStorage: ", "set key done");
+	});
 }
 
-function tileButtonClick(event) {
+// read storage for tile list and add them to HTML page
+function readFromStorage() {
+	var TilesInfoKey = 'TilesInfoKey';
+
+	chrome.storage.local.get([ TilesInfoKey ], function(jsonArrayStr) {
+		if (!jsonArrayStr.TilesInfoKey) {
+			logMessage("readFromStorage: ", "get key.TilesInfoKey=UNDEFINED");
+		} else {
+			var jsonArray = JSON.parse(jsonArrayStr.TilesInfoKey);
+			logMessage("readFromStorage: ", "jsonArray.length="
+					+ jsonArray.length);
+
+			addMultipleTiles(jsonArray);
+		}
+	});
+}
+
+// add tiles to HTML page, for given JSON array (tile-info objects)
+function addMultipleTiles(jsonArray) {
+	logMessage("addMultipleTiles: ", "jsonArray=" + jsonArray.length);
+
+	for (i = 0; i < jsonArray.length; i++) {
+		var tileInfo = jsonArray[i];
+		addOneTile(tileInfo.title, tileInfo.url);
+	}
+}
+
+// add a tile to HTML page
+function addOneTile(title, url) {
+	logMessage("addOneTile: ", "tileInfo: " + title + "- " + url);
+
+	if (title.length != 0 && url.length != 0) {
+
+		var tiles_grid = document.getElementById("tiles_grid");
+		var currButtons = tiles_grid.getElementsByClassName('tile_button');
+		logMessage("addOneTile: ", "currButtons=" + currButtons.length);
+
+		if (currButtons.length < 30) {
+			var button = document.createElement("button");
+			button.className = "tile_button";
+			button.value = url;
+			button.innerText = title;
+			button.addEventListener('click', process_tileButtonClick);
+
+			tiles_grid.appendChild(button);
+		} else {
+			logMessage("addOneTile: ", "ERROR - Tile limit of 30 reached");
+		}
+	}
+}
+
+// remove one tile from HTML page by matching title
+function removeOneTile(title) {
+	logMessage("removeOneTile: ", "title=" + title);
+	if (title.length != 0) {
+		var tiles_grid = document.getElementById("tiles_grid");
+		var currButtons = tiles_grid.getElementsByClassName('tile_button');
+		if (currButtons.length > 0) {
+			for (i = 0; i < currButtons.length; i++) {
+				var button = currButtons[i];
+				if (button.innerText == title) {
+					logMessage("removeOneTile: ", "FOUND @ " + i);
+					tiles_grid.removeChild(button);
+					break;
+				}
+			}
+		} else {
+			logMessage("removeOneTile: ", "ERROR - There are no tiles on page");
+		}
+	}
+}
+
+// handle tile-button press
+function process_tileButtonClick(event) {
 	var target = event.target || event.srcElement;
 	logMessage("TileButton_Click: ", target);
 	logMessage("TileButton_Click: ", target.value);
@@ -64,22 +116,44 @@ function tileButtonClick(event) {
 	});
 }
 
-function buttonClick(event) {
+// handle ADD, REMOVE button press
+function process_buttonClick(event) {
 	var target = event.target || event.srcElement;
+	logMessage("process_buttonClick: ", target);
+	logMessage("process_buttonClick: ", target.value);
 
 	if (target.value == "add") {
-		var form = document.getElementById("add_bookmark_form")
-		var text = document.createElement("textarea");
-		var button = document.createElement("button");
-		// form.add(text);
-		// form.add(button);
-		// add_bookmark_form
-	}
+		var inputTitle = document.getElementById("add_title").value;
+		var inputUrl = document.getElementById("add_url").value;
+		addOneTile(inputTitle, inputUrl);
+	} else if (target.value == "remove") {
+		var inputTitle = document.getElementById("add_title").value;
+		removeOneTile(inputTitle);
+	} else if (target.value == "save") {
+		var jsonArrayStr = "["
+		var tiles_grid = document.getElementById("tiles_grid");
 
-	logMessage("Button_Click: ", target);
-	logMessage("Button_Click: ", target.value);
+		var jsonArray = [];
+		var currButtons = tiles_grid.getElementsByClassName('tile_button');
+		for (i = 0; i < currButtons.length; i++) {
+			jsonArray.push({
+				"title" : currButtons[i].innerText,
+				"url" : currButtons[i].value
+			});
+		}
+
+		writeToStorage(jsonArray);
+	} else if (target.value == "clear") {
+		var tiles_grid = document.getElementById("tiles_grid");
+		while (tiles_grid.hasChildNodes()) {
+			tiles_grid.removeChild(tiles_grid.lastChild);
+		}
+	} else if (target.value == "bkp") {
+		readInputFile();
+	}
 }
 
+// handle loading of book-marks
 function process_bookmark(bookmarks) {
 	var select = document.getElementById("select_bookmarks")
 
@@ -97,11 +171,30 @@ function process_bookmark(bookmarks) {
 	}
 }
 
+// handle selection of book-mark from drop down list
 function process_listChange() {
 	var item = document.getElementById("select_bookmarks").value;
 	logMessage("List_Selected: ", item, "")
 }
 
+// read JSON file for tile list and add them to HTML page
+function readInputFile() {
+	var fileReadRequest = new XMLHttpRequest();
+	fileReadRequest.open("GET", 'url.json', true);
+	fileReadRequest.onreadystatechange = function() {
+		if (fileReadRequest.readyState === 4) {
+			var fileContents = fileReadRequest.responseText;
+			var jsonArray = JSON.parse(fileContents);
+			logMessage("readInputFile: ", "jsonArray=" + jsonArray.length);
+
+			addMultipleTiles(jsonArray);
+		}
+	}
+
+	fileReadRequest.send();
+}
+
+// simple logger
 function logMessage(prefix, postfix) {
 	var date = new Date();
 	var timeStamp = "";
